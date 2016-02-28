deb67bca5a83bf34974383d4ecc49f4a2020fb49
EwingKang <f039281310@yahoo.com.tw>
2016-02-28 15:35:23 +0800

    Update File comment Info

--- Dubins/dubins_curve.m
+++ Dubins/dubins_curve.m
@@ -11,7 +11,32 @@
 %       https://github.com/AndrewWalker/Dubins-Curves#shkel01
 %       Shkel, A. M. and Lumelsky, V. (2001). "Classification of the Dubins
 %                  set". Robotics and Autonomous Systems 34 (2001) 179¡V202
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Original Source: Andrew Walker
+% MATLAB-lization: Ewing Kang
+% Date: 2016.2.28
+% contact: f039281310 [at] yahoo.com.tw
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2016, Ewing Kang                                                 % 
+%                                                                                %
+% Permission is hereby granted, free of charge, to any person obtaining a copy   %
+% of this software and associated documentation files (the "Software"), to deal  %
+% in the Software without restriction, including without limitation the rights   %
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      %
+% copies of the Software, and to permit persons to whom the Software is          %  
+% furnished to do so, subject to the following conditions:                       %
+%                                                                                %
+% The above copyright notice and this permission notice shall be included in     %
+% all copies or substantial portions of the Software.                            %
+%                                                                                %
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     %
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       %
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    %
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         %
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  %
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN      %
+% THE SOFTWARE.                                                                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function path = dubins_curve(p1, p2, r, stepsize)
     close(findobj('type','figure','name','Dubins curve'));
     %%%%%%%%%%%%%%%%%%%%%%%%% DEFINE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
